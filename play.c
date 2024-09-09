565c87fd06cf804ee5dbd97105134a1b31ae8c65
telnorp <danielmaltchikov@protonmail.com>
2024-09-09 13:23:17 +0300

    Update play.c
    päivitetty logiikkaa ja lisätty enemmän asioita.

--- play.c
+++ play.c
@@ -3,37 +3,40 @@
 #include <time.h>
 
 int main() {
-    int a = -1;
-    int oikea;
+    int a, oikea, uudelleen = 1;
 
-    srand(time(0));
+    while (uudelleen == 1) {
+        a = -1;
+        srand(time(0));
+        oikea = rand() % 101;
 
-    oikea = rand() % 101;
+        printf("Tervetuloa peliin.\nArvaa numero 0-100. Onnea!\n");
 
-    printf("Tervetuloa peliin.\nArvaa numero 0-100. Onnea!\n");
+        while (a != oikea) {
+            printf("Kirjoita numero...\n");
 
-    while (a != oikea) {
-        printf("Kirjoita numero...\n");
+            if (scanf_s("%d", &a) != 1) {
+                printf("Vastaus ei ole numero!\n");
 
-        if (scanf_s("%d", &a) != 1) {
-            printf("Vastaus ei ole numero!\n");
+                while (getchar() != '\n');
+                continue;
+            }
 
-            
-            while (getchar() != '\n');
-            continue;  
+            if (a < 0 || a > 100) {
+                printf("Numero on oltava 0-100.\n");
+            }
+            else if (a < oikea) {
+                printf("Liian matala.\n");
+            }
+            else if (a > oikea) {
+                printf("Liian korkea.\n");
+            }
         }
 
-        if (a < 0 || a > 100) {
-            printf("Numero on oltava 0-100.\n");
-        }
-        else if (a < oikea) {
-            printf("Liian matala.\n");
-        }
-        else if (a > oikea) {
-            printf("Liian korkea.\n");
-        }
+        printf("Oikein!\nPaina 1 pelataksesi uudelleen, muu lopettaa...\n");
+        while (getchar() != '\n');
+        uudelleen = getchar() == '1' ? 1 : 0;
     }
 
-    printf("Oikein!\n");
     return 4919;
 }
